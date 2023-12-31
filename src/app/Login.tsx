e260d9b4ccc3207b76002e08bce9cae8e37dd3f3
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2023-12-31 00:39:50 -0500

    Update Login.tsx

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -59,7 +59,7 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="relative isolate h-screen overflow-hidden bg-gray-900">
+      <div className="relative isolate overflow-auto bg-gray-900">
         <img
           src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
           alt=""
@@ -163,34 +163,8 @@ export default function LoginPage() {
             </div>
           </div>
         </div>
-        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-          <svg
-            className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
-          >
-            <path
-              fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-              fillOpacity=".2"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#9089FC" />
-                <stop offset={1} stopColor="#FF80B5" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-
-      </div>
-      <a id="yourApkLink" target="_blank" rel="noopener noreferrer">
+     
+        <a id="yourApkLink" target="_blank" rel="noopener noreferrer">
       <div className="fixed -bottom-1 p-4  left-0 right-0 mx-auto rounded-2xl h-16 bg-white animate-bounce infinite flex items-center justify-center w-fit">
         <div className="flex flex-row items-center justify-evenly gap-4">
           <AndroidLogo size={32} weight="duotone" />
@@ -198,6 +172,8 @@ export default function LoginPage() {
         </div>
       </div>
       </a>
+      </div>
+    
     </>
   );
 }
